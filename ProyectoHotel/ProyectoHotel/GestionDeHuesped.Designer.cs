07ceb10547098d8cc72abe4923a14ad4598ea037
AlejandraC311003 <carranzaalejandra855@gmail.com>
2025-05-12 11:51:54 -0600

    Inicio de proyecto
    Se creo el proyecto en visual studio 2010 y estaremos usando el lenguaje c#

---
+++ ProyectoHotel/ProyectoHotel/GestionDeHuesped.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ProyectoHotel
+{
+    partial class Gestion_de_HUesped
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Gestion_de_HUesped
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 253);
+            this.Name = "Gestion_de_HUesped";
+            this.Text = "GestionDeHuesped";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
